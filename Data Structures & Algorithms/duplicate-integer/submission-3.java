002495c5eef9f8297893dbcb1762a004928421d5
pankajpandey0001 <i.pankajpandey0001@gmail.com>
2026-08-04 09:19:15 +0530

    Add: duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Set<Integer> dupli_check = new HashSet<>(nums.length);
+        for (int i:nums) {
+            if (dupli_check.add(i)==false) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
