673e2e97f226cd34226c42a1b21546cd9f88a436
pankajpandey0001 <i.pankajpandey0001@gmail.com>
2026-08-07 19:03:42 +0530

    Update: products-of-array-discluding-self - submission-0

--- Data Structures & Algorithms/products-of-array-discluding-self/submission-0.java
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.java
@@ -1,16 +1,24 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int[] prefix = new int[nums.length];
-        int[] suffix = new int[nums.length];
-        prefix[0] = 1;
-        suffix[nums.length-1] = 1;
-        for (int i=1; i<nums.length; i++) {
-            prefix[i] = prefix[i-1]*nums[i-1];
-            suffix[nums.length-i-1] = suffix[nums.length-i]*nums[nums.length-i];
+        int n = nums.length;
+        int[] lr_ans = new int[n];
+        int[] rl_ans = new int[n];
+        int[] result = new int[n];
+
+        lr_ans[0] = 1;
+        for (int i = 1; i < n; i++) {
+            lr_ans[i] = lr_ans[i - 1] * nums[i - 1];
         }
-        for (int i=0; i<nums.length; i++) {
-            nums[i] = prefix[i]*suffix[i];
+
+        rl_ans[n - 1] = 1;
+        for (int i = n - 2; i >= 0; i--) {
+            rl_ans[i] = rl_ans[i + 1] * nums[i + 1];
         }
-        return nums;
+
+        for (int i = 0; i < n; i++) {
+            result[i] = lr_ans[i] * rl_ans[i];
+        }
+
+        return result;
     }
-}  
+}
